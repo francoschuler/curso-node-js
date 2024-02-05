3960583d286054022610b938148218d8bf087913
Franco <franschuler13@gmail.com>
2024-02-05 07:25:59 +0100

    MVC applied

---
+++ clase-4/app.js
@@ -0,0 +1,19 @@
+import express, { json } from 'express'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+
+// import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
+const app = express()
+app.disable('x-powered-by')
+app.use(json())
+app.use(corsMiddleware())
+
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
